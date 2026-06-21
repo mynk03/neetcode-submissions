7bcbda74fadb6a03e82a5ba73773d5d59e6c7d9e
Mayank <119710987+mynk03@users.noreply.github.com>
2026-06-21 17:27:58 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set <int> s;
+
+        for ( int i : nums){
+            if(s.contains(i)) return true;
+            s.insert(i);
+        }
+
+        return false;
+        
+    }
+};
---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.cpp
@@ -0,0 +1,42 @@
+static const int _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string s = "";
+
+        if (strs.size() == 0) return "";
+        for(string e : strs){
+            int n = e.size();
+            string temp = format("{:03}", n); 
+            s = s+temp;
+            s = s+e;
+        }
+        return s;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        
+        int n = s.size();
+        if (n == 0) return {};
+        if (n < 3) return {};
+        int i = 0;
+        while (i < n) {
+            string lstr = s.substr(i, 3);
+            int l = stoi(lstr);
+
+            string d ;
+            d= s.substr(i+3,l);
+
+            ans.push_back(d);
+            i = i+l+3;
+        }
+
+        return ans;
+    }
+};
